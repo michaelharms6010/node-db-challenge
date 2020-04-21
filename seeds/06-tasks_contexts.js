294e574ca2e0b3f8a3d942c2efa882ff3b617d1b
Michael Harms <michael.harms6010@gmail.com>
2020-04-20 21:50:09 -0500

    some garbo

--- seeds/06-tasks_contexts.js
+++ seeds/06-tasks_contexts.js
@@ -7,6 +7,8 @@ exports.seed = function(knex) {
       return knex('task_contexts').insert([
         {task_id: 1, context_id: 1},
         {task_id: 1, context_id: 3},
+        {task_id: 2, context_id: 1},
+        {task_id: 2, context_id: 3},
         {task_id: 3, context_id: 1},
         {task_id: 3, context_id: 3},
         {task_id: 4, context_id: 1},
